06ff01ddfc045d7bf7d7157edf682109524624f4
jaamaarion-stack <jaamaarionn@gmail.com>
2026-06-05 05:37:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function sendMessage() {
-    alert("Chatbot coming soon!");
+
+    let input = document.getElementById("user-input");
+
+    let chatBox = document.getElementById("chat-box");
+
+    let message = input.value;
+
+    if(message === "") return;
+
+    chatBox.innerHTML +=
+        `<p><b>You:</b> ${message}</p>`;
+
+    chatBox.innerHTML +=
+        `<p><b>Bot:</b> Thanks for your message!</p>`;
+
+    input.value = "";
+
+    chatBox.scrollTop =
+        chatBox.scrollHeight;
 }
